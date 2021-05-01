1c8428fc7bddcbf6d01100d2ef7d9753705e5b83
dengpingan <dengpingan@novelpro.cn>
2021-05-01 19:41:30 +0800

    fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import json
 # load config  
 fileNamePath = os.path.split(os.path.realpath(__file__))[0]
 yamlPath = os.path.join(fileNamePath,'config.yaml')
-cf = yaml.load(open(yamlPath,'r',encoding='utf-8').read())
+cf = yaml.load(open(yamlPath,'r',encoding='utf-8').read(),Loader=yaml.FullLoader)
 
 # flagOptions > yaml
 parser = argparse.ArgumentParser(usage=" option can coverage .yaml ", description="")
@@ -19,6 +19,7 @@ parser.add_argument("-t","--to", default=cf['To'], help="到达站", dest="to")
 parser.add_argument("-d","--date", default=cf['Date'], help="订票日期", dest="date")
 parser.add_argument("-lbt","--lbt", default=cf['Customization']['LatestBusTime'], help="最晚开车时间", dest="lbt")
 parser.add_argument("-lst","--lst", default=cf['Customization']['LatestShipTime'], help="最晚开船时间", dest="lst")
+parser.add_argument("-mst","--mst", default=cf['Customization']['MinShipTime'], help="最早开船时间", dest="mst")
 parser.add_argument("-line","--line", default=cf['Customization']['LineNum'], help="指定航班", dest="line")
 parser.add_argument("-class","--class", default=cf['Customization']['Class'], help="指定舱位", dest="className")
 args = parser.parse_args()
@@ -36,18 +37,24 @@ def notice(sss):
         return
     pyload = {"msgtype": "text", "text": {"content":json.dumps(sss,sort_keys=True, indent=2).encode('utf-8').decode('unicode_escape')}}
     requests.post(cf['Notice']['companyWx'], data=json.dumps(pyload),headers={"Content-Type": "application/json; charset=UTF-8"})
-
+# notice("script running  ")
 code = 0
 errors = 0
-while code != 200 and errors<10:
+tryTimes = 0
+login_res = requests.post('https://www.ssky123.com/api/v2/user/passLogin?phoneNum=' + account['phoneNum'] + '&passwd=' + account['passwd'] + '&deviceType=2').json()
+passengers = account['passengers']
+userid = login_res['data']['userId']
+token = login_res['data']['token']
+while code != 200 and errors<3: 
+    tryTimes+=1
+    if tryTimes%100==0:#刷新token
+        login_res = requests.post('https://www.ssky123.com/api/v2/user/passLogin?phoneNum=' + account['phoneNum'] + '&passwd=' + account['passwd'] + '&deviceType=2').json()
+        token = login_res['data']['token']
     # time.sleep(1)
     print('... ....')
-    login_res = requests.post('https://www.ssky123.com/api/v2/user/passLogin?phoneNum=' + account['phoneNum'] + '&passwd=' + account['passwd'] + '&deviceType=2').json()
-    passengers = account['passengers']
 
     # print('===================Login Info============================')
-    userid = login_res['data']['userId']
-    token = login_res['data']['token']
+    
 
     def chekcToken():
         requests.get('https://www.ssky123.com/api/v2/user/tokenCheck', headers={'authentication': account['authentication'], 'token': token}).json()
@@ -96,6 +103,8 @@ while code != 200 and errors<10:
             continue
         if args.lst!="" and tr['sailTime']!="" and tr['sailTime']>args.lst:
             continue
+        if args.mst!="" and tr['sailTime']!="" and tr['sailTime']<args.mst:
+            continue
         if args.line!='' and (tr['lineNum']+tr['sx'])!=args.line:
             continue
         ii = 0
@@ -137,10 +146,13 @@ while code != 200 and errors<10:
         order['totalPayFee'] = seat['totalPrice'] * len(passengers)
         order['sailDate'] = args.date
         res = post('https://www.ssky123.com/api/v2/holding/save', order)
+        code = res['code']
         pprint(res)
         notice(res)
-        code = res['code']
-        errors+=1
+        print(time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime()))
+        if code !=200 and code!=300:
+            errors+=1
+            break
 
 
 
